fc72a76d0554bc75ab9e00cb1e619a141aa7fbae
Angelica <ageliki.th@gmail.com>
2022-04-07 21:13:22 +0300

    subtask 1

--- cMeans.py
+++ cMeans.py
@@ -8,19 +8,18 @@ class cMeans:
         # Extracting X
         X = data[:,:-2]
 
-        # Adding bias column x0
-        X = np.column_stack((np.ones(X.shape[0]), X))
-
         # Extracting y
         goals = data[:,-2:]
         results = goals[:,0] - goals[:,1]
         y = np.array([[1,0,0] if result > 0 else [0,1,0] if result == 0 else [0,0,1] for result in results])
-        return y
+        return X
 
 
-    def cluster(self, X, clusters=3):
+    def cluster(self, X, label, clusters=3):
         kmeans = KMeans(n_clusters=clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
-        pred_y = kmeans.fit_predict(X)
-        plt.scatter(X[:,0], X[:,1])
-        plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
+        cl = kmeans.fit_predict(X)
+        plt.scatter(X[:,0], X[:,1],c=cl,cmap='rainbow')
+        plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='black')
+        plt.title(label)
         plt.show()
+
